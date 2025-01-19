71cee1820adb6217385618138db13da84433051c
Oleksandr Volkohon <volkogon212@gmail.com>
2025-01-19 18:59:43 +0200

    Create ua.ts

---
+++ src/i18n/languages/ua.ts
@@ -0,0 +1,38 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const en: Translation = {
+  [Key.home]: 'Дім',
+  [Key.about]: 'Про нас',
+  [Key.archive]: 'Архів',
+  [Key.search]: 'Пошук',
+
+  [Key.tags]: 'Теги',
+  [Key.categories]: 'Категорії',
+  [Key.recentPosts]: 'Нещодавні новини',
+
+  [Key.comments]: 'Коментарії',
+
+  [Key.untitled]: 'Без назви',
+  [Key.uncategorized]: 'Без категорії',
+  [Key.noTags]: 'Без тегів',
+
+  [Key.wordCount]: 'слово',
+  [Key.wordsCount]: 'слів',
+  [Key.minuteCount]: 'хвилину',
+  [Key.minutesCount]: 'хвилин',
+  [Key.postCount]: 'пост',
+  [Key.postsCount]: 'постів',
+
+  [Key.themeColor]: 'Колір теми',
+
+  [Key.lightMode]: 'Світлий',
+  [Key.darkMode]: 'Темний',
+  [Key.systemMode]: 'Системний',
+
+  [Key.more]: 'Більше',
+
+  [Key.author]: 'Автор',
+  [Key.publishedAt]: 'Опубліковано о',
+  [Key.license]: 'Ліцензія',
+}
